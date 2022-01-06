a156b973a5dce998b5d59ee8882dc6e8d823f80f
unknown <hibit_at@hotmail.com>
2022-01-06 18:05:13 +0900

    css paging

--- app/forms.py
+++ app/forms.py
@@ -2,4 +2,7 @@ from django import forms
 
 class Filter(forms.Form):
     word = forms.CharField(required=False)
-    free_only = forms.BooleanField(required=False)
+    free_only = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(attrs={'style':'width:20px;height:20px;'}),
+        )

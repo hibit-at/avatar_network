6920d4b23018bbb4b81258dd982a614498e79355
unknown <hibit_at@hotmail.com>
2022-02-22 13:54:40 +0900

    avata item blank

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.db.models.fields import related
 
 # Create your models here.
 
+
 class Creator(models.Model):
     creator_id = models.CharField(max_length=200, primary_key=True)
     creator_name = models.CharField(max_length=200)
@@ -11,23 +12,27 @@ class Creator(models.Model):
     def __str__(self):
         return self.creator_name
 
+
 class Avatar(models.Model):
     avatar_id = models.IntegerField(primary_key=True)
     avatar_name = models.CharField(max_length=200)
     imageURL = models.CharField(max_length=200)
-    creator = models.ForeignKey(Creator, on_delete=models.CASCADE, related_name='avatars')
+    creator = models.ForeignKey(
+        Creator, on_delete=models.CASCADE, related_name='avatars')
     price = models.IntegerField()
     created_at = models.DateTimeField()
 
     def __str__(self):
         return self.avatar_name
 
+
 class Item(models.Model):
     item_id = models.IntegerField(primary_key=True)
     item_name = models.CharField(max_length=200)
-    avatar = models.ManyToManyField(Avatar, related_name='items')
+    avatar = models.ManyToManyField(Avatar, related_name='items', blank=True)
     imageURL = models.CharField(max_length=200)
-    creator = models.ForeignKey(Creator, on_delete=models.CASCADE, related_name='items')
+    creator = models.ForeignKey(
+        Creator, on_delete=models.CASCADE, related_name='items')
     price = models.IntegerField()
     created_at = models.DateTimeField()
 

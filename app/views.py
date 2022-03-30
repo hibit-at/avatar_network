adc11b3ee7e839ea240134e4d545bd93915cd1d4
unknown <hibit_at@hotmail.com>
2022-03-30 12:19:16 +0900

    folder

--- app/views.py
+++ app/views.py
@@ -361,7 +361,7 @@ def userpage(request, tid=''):
                     params['error'] = 'ERROR : 空のフォルダがある状態では新規フォルダは作成できません。'
                     return render(request, 'userpage.html', params)
             count = Folder.objects.filter(editor=customer).count()
-            if count > 1 and not customer.isSupporter:
+            if count > 0 and not customer.isSupporter:
                 params['error'] = 'ERROR : 複数のフォルダを作成するには、サポーターになる必要があります。'
                 return render(request, 'userpage.html', params)
             Folder.objects.create(

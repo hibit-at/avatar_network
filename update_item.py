f0e1aa11928f02fe5a579acc52b99522c55ddf97
unknown <hibit_at@hotmail.com>
2023-03-13 11:40:50 +0900

    scraping change

--- update_item.py
+++ update_item.py
@@ -42,10 +42,10 @@ def process(force=False):
         pat = r'https://[0-9a-zA-Z_\-]+.booth.pm/items/(\d+)'
         link_ids2 = re.findall(pat, main_txt)
         link_ids.extend(link_ids2)
-        pat = r'<p class="autolink break-words font-noto-sans typography-16 whitespace-pre-line">(.*?)</section></div>'
+        pat = r'<p class="autolink break-words font-noto-sans typography-16 whitespace-pre-line">(.*?)(</div></section>|</section></div>)'
         if len(re.findall(pat, txt)) > 0:
             print("hit in the second step")
-            scr_txt = re.findall(pat, txt)[0]
+            scr_txt = re.findall(pat, txt)[0][0]
             pat = r'https://booth.pm/(.*?)/items/(\d+)'
             link_ids3 = re.findall(pat, scr_txt)
             link_ids3 = [L[1] for L in link_ids3]
